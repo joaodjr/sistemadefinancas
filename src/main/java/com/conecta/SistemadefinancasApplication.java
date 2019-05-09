a736418831f268dbbefc9ca79d03c9d4b53c923d
Joao Leite <joao_leite@hotmail.com.br>
2019-05-09 00:43:41 -0300

    primeiro commit

---
+++ src/main/java/com/conecta/SistemadefinancasApplication.java
@@ -0,0 +1,13 @@
+package com.conecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadefinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadefinancasApplication.class, args);
+	}
+
+}
